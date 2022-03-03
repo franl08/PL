58f93df4df937c9dedf09914b88911db95372e78
franl08 <franl08@github.com>
2022-03-03 10:18:14 +0000

    oliver benji

--- Ficha Proposta 01/matriculas.py
+++ Ficha Proposta 01/matriculas.py
@@ -1,4 +1,12 @@
 import re
-import sys
 
-m = re.compile(r'^(\d{2}\.{3}\d{2}\.{3}\d{2}\.{3}\d{2})|(\d{2}-\d{2}-\d{2}-\d{2})|(\d{2}:\d{2}:\d{2}:\d{2})$')
+m = re.compile(r'(\d{2}\.{3}\d{2}\.{3}\d{2}\.{3}\d{2})|(\d{2}-\d{2}-\d{2}-\d{2})|(\d{2}:\d{2}:\d{2}:\d{2})')
+f = open('texto.txt', 'r')
+nMatriculas = 0
+
+for line in f:
+    lista = m.findall(line)
+    if(lista):
+        nMatriculas += 1
+
+print("Encontradas %d matrículas" % (nMatriculas))
